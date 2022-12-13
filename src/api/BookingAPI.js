106ddcf302ea994812f803971c65a972e91b707e
hieu <hieuqbh15@gmail.com>
2022-12-13 16:53:25 +0700

    delete booking

--- src/api/BookingAPI.js
+++ src/api/BookingAPI.js
@@ -3,6 +3,10 @@ import http from './http'
 const BookingAPI = {
     HistoryBooking( config) {
         return http.get('/my-account/history-booking/', config)
+    },
+    DeleteHistoryBooking(id,config){
+        return http.delete(`/booking-pitches/${id}`,null, config)
     }
+    
 }
 export default BookingAPI
